1b8656a7700ae5973b568fda6afe55f6fd2da7b0
Diego Caucota <diegocaucota@gmail.com>
2025-07-31 01:42:04 -0300

    add: data songsTest

---
+++ src/data/songsTest.js
@@ -0,0 +1,112 @@
+export const songsData = [
+    {
+        id: '456ghj',
+        titulo: 'Blinding Lights',
+        artista: 'The Weeknd',
+        album: 'After Hours',
+        genero: 'Pop',
+        imagen: 'https://res.cloudinary.com/duwi53e7z/image/upload/v1753838253/the_weeknd_after_hours-portada_d3vjxo.jpg',
+        duracion: '03:20',
+        audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'
+    },
+    {
+        id: '2345kjhk',
+        titulo: 'Bohemian Rhapsody',
+        artista: 'Queen',
+        album: 'A Night at the Opera',
+        genero: 'Rock',
+        imagen: 'https://res.cloudinary.com/duwi53e7z/image/upload/v1753838294/queen-la-banda-color-portada-disco_jliakh.jpg',
+        duracion: '05:45',
+        audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3'
+    },
+    {
+        id: '789klm',
+        titulo: 'Stairway to Heaven',
+        artista: 'Led Zeppelin',
+        album: 'Led Zeppelin IV',
+        genero: 'Rock',
+        imagen: 'https://res.cloudinary.com/duwi53e7z/image/upload/v1753838374/Led_Zeppelin_-_Led_Zeppelin_IV_zpsn36.jpg',
+        duracion: '08:02',
+        audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3'
+    },
+    {
+        id: '101nop',
+        titulo: 'Shape of You',
+        artista: 'Ed Sheeran',
+        album: '÷',
+        genero: 'Pop',
+        imagen: 'https://res.cloudinary.com/duwi53e7z/image/upload/v1753838412/Divide_cover_pdsol6.png',
+        duracion: '03:53',
+        audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3'
+    },
+    {
+        id: '112qrs',
+        titulo: 'Strobe',
+        artista: 'Deadmau5',
+        album: 'For Lack of a Better Name',
+        genero: 'Electronica',
+        imagen: 'https://res.cloudinary.com/duwi53e7z/image/upload/v1753838516/ForLackofaBetterNameAlbum_msgxoa.jpg',
+        duracion: '10:37',
+        audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3'
+    },
+    {
+        id: '131tuv',
+        titulo: 'Zamba para olvidar',
+        artista: 'Mercedes Sosa',
+        album: 'Alta fidelidad',
+        genero: 'Folclore',
+        imagen: 'https://res.cloudinary.com/duwi53e7z/image/upload/v1753838574/ab67616d0000b273ef3b7e0e077a821abbac6bf6_c15psy.jpg',
+        duracion: '04:15',
+        audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3'
+    },
+    {
+        id: '415wxy',
+        titulo: 'Take Five',
+        artista: 'Dave Brubeck Quartet',
+        album: 'Time Out',
+        genero: 'Jazz',
+        imagen: 'https://res.cloudinary.com/duwi53e7z/image/upload/v1753838610/Davebrubeckquartet1967a_ft566x.jpg',
+        duracion: '05:24',
+        audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3'
+    },
+    {
+        id: '161zab',
+        titulo: 'SAD!',
+        artista: 'XXXTentacion',
+        album: '?',
+        genero: 'Trap',
+        imagen: 'https://res.cloudinary.com/duwi53e7z/image/upload/v1753838643/MV5BZjEwYTRjYzAtZjllNi00OTRiLWFkYzAtNzc4ZWQxZmQ1MzUwXkEyXkFqcGc._V1_FMjpg_UX1000__jxaxo8.jpg',
+        duracion: '02:46',
+        audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3'
+    },
+    {
+        id: '718cde',
+        titulo: 'Sweet Child O\' Mine',
+        artista: 'Guns N\' Roses',
+        album: 'Appetite for Destruction',
+        genero: 'Rock',
+        imagen: 'https://res.cloudinary.com/duwi53e7z/image/upload/v1753838676/Guns-N-Roses_-Indierocks-2nyoxl93rx9mskd24mxwzuf8bj616ylqx9pr4uiz4457gttgnpre2kbcjbsbb4_nwvax2.jpg',
+        duracion: '05:56',
+        audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3'
+    },
+    {
+        id: '192fgh',
+        titulo: 'Billie Jean',
+        artista: 'Michael Jackson',
+        album: 'Thriller',
+        genero: 'Pop',
+        imagen: 'https://res.cloudinary.com/duwi53e7z/image/upload/v1753838836/thriller-arte-de-tapa-1_nxkbxx.png',
+        duracion: '04:54',
+        audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3'
+    },
+    {
+        id: '293ijk',
+        titulo: 'One More Time',
+        artista: 'Daft Punk',
+        album: 'Discovery',
+        genero: 'Electronica',
+        imagen: 'https://res.cloudinary.com/duwi53e7z/image/upload/v1753838876/49b2de4504144d8f8ccdffee9b2fba71_ywwahn.jpg',
+        duracion: '05:20',
+        audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-11.mp3'
+    }
+];
