adf54368e4f4fe2ca4927ad18f90035505363517
Diego Caucota <diegocaucota@gmail.com>
2025-07-31 02:11:08 -0300

    fix: components pages About

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -6,14 +6,14 @@ import omarmattos from "../../assets/omarmatt01.png"
 const About = () => {
     return (
         <Container className="my-5">
-            <h1 className='text-center fs-1'>Acerca de About</h1>
+            <h1 className='text-center fs-1'>Nosotros</h1>
             <h3 className="text-center my-4">"Con pasión y código, nuestro equipo de desarrolladores construyó una página de música donde cada acorde y melodía cobra vida."</h3>
             <Row>
                 <Col md={4} lg={4} className="my-3">
                 <Card className="w-100">
                     <img src={omarmattos} alt="Omar Mattos" className="img-fluid card-img-top"/>
                     <Card.Body>
-                        <Card.Title className="text-center fs-4">Omar Oscar Alfredo Mattos</Card.Title>
+                        <Card.Title className="text-center fs-4">Diego Caucota</Card.Title>
                     </Card.Body>
                 </Card>
                 </Col>
@@ -21,7 +21,7 @@ const About = () => {
                 <Card className="w-100">
                     <img src={santilacki} alt="Santiago Lacki" className="img-fluid card-img-top"/>
                     <Card.Body>
-                        <Card.Title className="text-center fs-4">Santiago Lacki Sinclair</Card.Title>
+                        <Card.Title className="text-center fs-4">Gisela Ortiz</Card.Title>
                     </Card.Body>
                 </Card>
                 </Col>
@@ -29,7 +29,7 @@ const About = () => {
                 <Card className="w-100">
                     <img src={santigonz} alt="Santiago Gonzalez" className="img-fluid card-img-top"/>
                     <Card.Body>
-                        <Card.Title className="text-center fs-4">Santiago Uriel Gonzalez</Card.Title>
+                        <Card.Title className="text-center fs-4">Astor Piazolla</Card.Title>
                     </Card.Body>
                 </Card>
                 </Col>
