2cb2b5645b695e04d54a19f048385cccc86d6762
Diego Caucota <diegocaucota@gmail.com>
2025-08-01 02:25:45 -0300

    fix: components routes ProtectorAdmin documentation

--- src/components/routes/ProtectorAdmin.jsx
+++ src/components/routes/ProtectorAdmin.jsx
@@ -1,5 +1,11 @@
 import { Navigate, Outlet } from "react-router";
 
+// Componente ProtectorAdmin que verifica si el usuario es administrador
+// Si no es administrador, redirige al usuario a la página de inicio
+// Si es administrador, permite el acceso a las rutas hijas
+// Utiliza Navigate para redirigir y Outlet para renderizar las rutas hijas
+// Este componente es útil para proteger rutas que solo deben ser accesibles por administradores
+// Recibe una prop isAdmin que indica si el usuario es administrador
 const ProtectorAdmin = ({isAdmin}) => {
 
     if(!isAdmin){
